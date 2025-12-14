3e1ac2f98f77c884dfd4d2c9af27b672880498b2
yuki2737 <youxiy377@gmail.com>
2025-12-14 16:11:33 +0900

    fix: persist firebase auth session

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -14,6 +14,9 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const fireAuth = getAuth(app);
+
+// ⭐️ これが超重要（ログイン状態を localStorage に保存）
+setPersistence(fireAuth, browserLocalPersistence);
